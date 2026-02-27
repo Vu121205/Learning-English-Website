65c90d1e7a5f768b204c2e8e8b3cb60561bd64aa
vuhoccode <vu03092019@gmail.com>
2026-02-27 21:36:19 +0700

    final

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cách chạy project
+
+1. Cài Python
+2. Cài MySQL
+3. Tạo database tên Learn_English_Website mật khẩu 123456 (tự nghiên cứu nha anh em)
+4. pip install -r requirements.txt (gõ vào terminal, chọn qua màn hình command prompt rồi gõ lệnh này để tải thư viện cần thiết về)
+5. python app.py (sau khi download xong thì gõ lệnh này)
--- controllers/admin/DashBoardController.py
+++ controllers/admin/DashBoardController.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, Flask, app, render_template
 
+from models.topic import Topic
+from models.vocabulary import Vocabulary
+
 dashBoard_bp = Blueprint('dashboard', __name__)
 
 @dashBoard_bp.route('/admin')
 def home():
-    return render_template('admin/dashboard.html')
+    vocab_count = Vocabulary.query.count()
+    topic_count = Topic.query.count()
+    return render_template('admin/dashboard.html', vocab_count=vocab_count, topic_count=topic_count)
